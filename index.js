6ebbfcb8b5b8d85d0a065e1dc9433189a312723e
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-03-26 16:30:15 +0100

    Explored more typescripts aspects (unions, omits, generics...)

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-var menu = [
-    { id: 1, name: "Margherita", price: 8 },
-    { id: 2, name: "Pepperoni", price: 10 },
-    { id: 3, name: "Hawaiian", price: 10 },
-    { id: 4, name: "Veggie", price: 9 }
-];
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var nextPizzaID = 1;
+var menu = [];
+addNewPizza({ name: "Margherita", price: 8 });
+addNewPizza({ name: "Pepperoni", price: 10 });
+addNewPizza({ name: "Hawaiian", price: 10 });
+addNewPizza({ name: "Veggie", price: 9 });
 var cashInRegister = 100;
 var nextOrderID = 1;
 var orderQueue = [];
 function addNewPizza(newPizza) {
-    menu.push(newPizza);
+    var pizza = __assign({ id: nextPizzaID }, newPizza);
+    menu.push(pizza);
+    return pizza;
 }
 function placeOrder(pizzaNameToOrder) {
     var targetPizza = menu.find(function (pizzaObj) { return pizzaObj.name === pizzaNameToOrder; });
@@ -31,9 +44,19 @@ function completeOrder(orderID) {
     targetOrder.status = "completed";
     return targetOrder;
 }
-addNewPizza({ id: 4, name: "Chicken Bacon Ranch", price: 12 });
-addNewPizza({ id: 5, name: "BBQ Chicken", price: 12 });
-addNewPizza({ id: 6, name: "Spicy Sausage", price: 11 });
+function getPizzaDetail(identifier) {
+    var targetPizza;
+    if (typeof identifier === "string") {
+        targetPizza = menu.find(function (pizzaObj) { return pizzaObj.name.toLowerCase() === identifier.toLowerCase(); });
+    }
+    else if (typeof identifier === "number") {
+        targetPizza = menu.find(function (pizzaObj) { return pizzaObj.id === identifier; });
+    }
+    return targetPizza;
+}
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
+addNewPizza({ name: "BBQ Chicken", price: 12 });
+addNewPizza({ name: "Spicy Sausage", price: 11 });
 placeOrder("Chicken Bacon Ranch");
 completeOrder(1);
 console.log("Menu:", menu);
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 ﻿type Pizza = {
-    id: number;
+    id?: number;
     name: string,
     price: number
 }
@@ -10,23 +10,28 @@ type Order = {
     status: "ordered" | "completed"
 }
 
-const menu : Pizza[] = [
-    { id: 1, name: "Margherita", price: 8 },
-    { id: 2, name: "Pepperoni", price: 10 },
-    { id: 3, name: "Hawaiian", price: 10 },
-    { id: 4, name: "Veggie", price: 9 }
-]
+let nextPizzaID = 1
+const menu : Pizza[] = []
+addNewPizza({name: "Margherita", price: 8 })
+addNewPizza({name: "Pepperoni", price: 10 })
+addNewPizza({name: "Hawaiian", price: 10 })
+addNewPizza({name: "Veggie", price: 9 })
 
 let cashInRegister = 100
 
 let nextOrderID = 1
 const orderQueue : Order[] = []
 
-function addNewPizza(newPizza: Pizza) {
-    menu.push(newPizza)
+function addNewPizza(newPizza: Omit<Pizza, "id">) : Pizza {
+    const pizza : Pizza = {
+        id: nextPizzaID,
+        ...newPizza
+    }
+    menu.push(pizza)
+    return pizza
 }
 
-function placeOrder(pizzaNameToOrder : string) {
+function placeOrder(pizzaNameToOrder : string) : Order {
     let targetPizza : Pizza = menu.find((pizzaObj: { name: string; }) => pizzaObj.name === pizzaNameToOrder);
     if (!targetPizza) {
         console.error(`Pizza ${pizzaNameToOrder} not found`)
@@ -41,7 +46,7 @@ function placeOrder(pizzaNameToOrder : string) {
     return orderPizzaObj;
 }
 
-function completeOrder(orderID: number) {
+function completeOrder(orderID: number) : Order {
     let targetOrder = orderQueue[orderID];
     if (!targetOrder) {
         console.error(`Order ${orderID} not found`);
@@ -52,9 +57,22 @@ function completeOrder(orderID: number) {
     return targetOrder
 }
 
-addNewPizza({id: 4, name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({id: 5, name: "BBQ Chicken", price: 12 })
-addNewPizza({id: 6, name: "Spicy Sausage", price: 11 })
+function getPizzaDetail(identifier : string | number) : Pizza | undefined {
+    let targetPizza : Pizza;
+
+    if (typeof identifier === "string") {
+        targetPizza = menu.find((pizzaObj: { name: string; }) => pizzaObj.name.toLowerCase() === identifier.toLowerCase() );
+    }
+    else if (typeof identifier === "number") {
+        targetPizza = menu.find((pizzaObj: { id: number }) => pizzaObj.id === identifier );
+    }
+    
+    return targetPizza;
+}
+
+addNewPizza({name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({name: "BBQ Chicken", price: 12 })
+addNewPizza({name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder(1)
